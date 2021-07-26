2bc075ad5b1358226b9811104e5fc3971a81e014
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-07-26 15:14:53 -0500

    Get Steps by project id

---
+++ HomePRO/Controllers/StepController.cs
@@ -0,0 +1,52 @@
+﻿using HomePRO.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HomePRO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StepController : ControllerBase
+    {
+        private readonly IStepRepository _stepRepository;
+        public StepController(IStepRepository stepRepository) {
+            _stepRepository = stepRepository;
+        }
+        // GET: api/<StepController>
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_stepRepository.GetStepById(id));
+        }
+
+        // GET api/<StepController>/5
+        [HttpGet("ProjectSteps/{id}")]
+        public IActionResult GetByProjectId(int id)
+        {
+            return Ok(_stepRepository.StepsByProjectId(id));
+        }
+
+        // POST api/<StepController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StepController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StepController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ HomePRO/Repositories/StepRepository.cs
@@ -0,0 +1,85 @@
+﻿using HomePRO.Models;
+using HomePRO.Utils;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomePRO.Repositories
+{
+    public class StepRepository : BaseRepository, IStepRepository
+    {
+        public StepRepository(IConfiguration configuration) : base(configuration) { }
+        public List<Step> StepsByProjectId(int id)
+        {
+
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"select Id, TimeEstimate, ProjectId, Step, Description, IsComplete  
+                                        From [Step]
+                                        where ProjectId = @id 
+                                        order by Step";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Step> steps = new List<Step>() { };
+                    while (reader.Read())
+                    {
+                        Step step = new Step()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            IsComplete = reader.GetBoolean(reader.GetOrdinal("IsComplete")),
+                            Description = DbUtils.GetString(reader, "Description"),
+                            StepNumber = DbUtils.GetInt(reader, "Step"),
+                            ProjectId = DbUtils.GetInt(reader, "ProjectId"),
+                            TimeEstimate = DbUtils.GetNullableDateTime(reader, "TimeEstimate"),
+                        };
+                        steps.Add(step);
+                    }
+                    reader.Close();
+                    return steps;
+                }
+            }
+        }
+        public void EditStep(Step step)
+        { }
+
+        public void DeleteStepByStepId(int id) { }
+        public void DeleteStepsByProjectId(int id) { }
+
+        public void AddStep(Step step) { }
+        public Step GetStepById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"select Id, TimeEstimate, ProjectId, Step, Description, IsComplete  from step where ProjectId = @id order by Step";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Step step = null;
+                    while (reader.Read())
+                    {
+                        step = new Step()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            IsComplete = reader.GetBoolean(reader.GetOrdinal("IsComplete")),
+                            Description = DbUtils.GetString(reader, "Description"),
+                            StepNumber = DbUtils.GetInt(reader, "Step"),
+                            ProjectId = DbUtils.GetInt(reader, "ProjectId"),
+                            TimeEstimate = DbUtils.GetNullableDateTime(reader, "TimeEstimate"),
+                        };
+                    }
+                    reader.Close();
+                    return step;
+                }
+
+            }
+        }
+    }
+}
