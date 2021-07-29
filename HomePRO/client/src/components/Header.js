ab8bd4d2e7565740ac9e82ae1bc68866d5d15066
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-07-29 15:59:19 -0500

    test

--- HomePRO/client/src/components/Header.js
+++ HomePRO/client/src/components/Header.js
@@ -43,6 +43,7 @@ export default function Header({ isLoggedIn }) {
                             <Redirect to="/register"></Redirect>
                         </NavLink>
                     </div>
+                    // something
                 }
             </div>
         </div >)
